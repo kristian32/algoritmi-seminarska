4389757d09d8fecad0f0ca20e2b1d7742daa765a
Kristian Šenk <kristian.senk32@gmail.com>
2020-05-02 17:32:35 +0200

    Added graphs

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from pylab import *
+import matplotlib.pyplot as plt
 import time
 from L_1 import L1
 from L_2 import L2
 
-def measure_time(P, metric):
+
+def calculate(P, metric):
     start = time.time()
     print(metric.name())
     print("Given set of ", len(P), " points")
@@ -13,19 +16,74 @@ def measure_time(P, metric):
     end = time.time()
     print("Time elapsed: ", end - start, "\n")
 
+    return S, end - start
 
-P1 = {(4, 1), (4, 2), (4, 3), (4, 4),
+
+def show_plot(points_x, points_y, Sx, Sy, title="", xlabel="", ylabel=""):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    scatter(points_x, points_y, s=100, marker='o', color='b')
+    scatter(Sx, Sy, s=100, marker='o', color='r')
+
+    left, right = -0.5, max(ax.get_xlim())
+    low, high = -0.5,  max(ax.get_ylim())
+
+    arrow(left, 0, right - left + 1, 0, length_includes_head=True, head_width=0.15)
+    arrow(0, low, 0, high - low + 1, length_includes_head=True, head_width=0.15)
+
+    plt.xlim(left, right + 1)
+    plt.ylim(low, high + 1)
+
+    plt.title(title, y=1.08)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+    grid()
+    show()
+
+
+def get_x_y_values(P):
+    Px, Py = [], []
+    for p in P:
+        Px.append(p[0])
+        Py.append(p[1])
+    return Px, Py
+
+
+def show_measured_time_by_P_size():
+    P_length = [len(p) for p, p_name in P]
+    for i in range(len(measured_times[::len(P)])):
+        measured_for_metric = measured_times[i::len(metrics)]
+        show_plot(measured_for_metric, P_length, [], [], metrics[i].name(), "time [ms]",
+                  "size of P")  # show elapsed time based on size of P
+
+
+P1 = [(4, 1), (4, 2), (4, 3), (4, 4),
       (3, 1), (3, 2), (3, 3), (3, 4),
       (2, 1), (2, 2), (2, 3), (2, 4),
-              (1, 2)}
+              (1, 2)]
 
-P2 = {(4, 1), (4, 2), (4, 3), (4, 4),
-      (3, 1), (3, 2)}
+P2 = [(4, 1), (4, 2), (4, 3), (4, 4),
+      (3, 1), (3, 2)]
 
-P3 = {(4, 1), (4, 2), (4, 3), (4, 4),
+P3 = [(4, 1), (4, 2), (4, 3), (4, 4),
       (3, 1), (3, 2), (3, 3), (3, 4),
-      (2, 1), (2, 2), (2, 3), (2, 4)}
+      (2, 1), (2, 2), (2, 3), (2, 4)]
 
-measure_time(P1, L1())
-# measure_time(P2, L1())
-# measure_time(P3, L1())
+P = [(P1, "P1"), (P2, "P2"), (P3, "P3")]  # TODO add more samples
+metrics = [L1(), L2()]  # TODO add L_inf
+
+measured_times = []
+S = []
+
+for p, p_name in P:
+    px, py = get_x_y_values(p)
+    for metric in metrics:
+        s, t = calculate(p, metric)
+        sx, sy = get_x_y_values(s)
+        show_plot(px, py, sx, sy, metric.name() + " on " + p_name)  # show all points and selected ones on plot
+        measured_times.append(t*1000)  # time in ms
+        S.append(s)
+
+show_measured_time_by_P_size()
