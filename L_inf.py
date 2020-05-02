99ad34327a1f6f454a4589dcdc557a19477328c0
Samo <samo.metlicar@hotmail.com>
2020-05-02 17:57:05 +0200

    Update L_inf.py
    Združil `case23left` in `case23right` v `case23`.

--- L_inf.py
+++ L_inf.py
@@ -23,9 +23,9 @@ def getLowHigh(xs, dim=0):
     return low, high
 
 def getBestDist(l, h, pa, pl, ph, pd, dim=0):
-    dist_lh = min(min(l[dim],h[dim])-pa[dim], h[1-dim] - l[1-dim])
-    dist_lph = min(min(l[dim],ph[dim])-pa[dim], ph[1-dim]-l[1-dim])
-    dist_plh = min(min(pl[dim],h[dim])-pa[dim], h[1-dim]-pl[dim])
+    dist_lh = min(min(abs(pa[dim]-l[dim]), abs(pa[dim]-h[dim])), h[1-dim]-l[1-dim])
+    dist_lph = min(min(abs(pa[dim]-l[dim]), abs(pa[dim]-ph[dim])), ph[1-dim]-l[1-dim])
+    dist_plh = min(min(abs(pa[dim]-pl[dim]), abs(pa[dim]-h[dim])), h[1-dim]-pl[1-dim])
     dist = max(pd, dist_lh, dist_lph, dist_plh)
     if dist_lh == dist:
         # l in h sta nova najbolja izbira
@@ -71,7 +71,7 @@ def case23left(P, dim=0):
     pa = min(P, key=lambda x: x[dim])
     pl = min(P, key=lambda x: (x[1-dim], -x[dim]))
     ph = max(P, key=lambda x: (x[1-dim], x[dim]))
-    pd = min(min(pl[dim], ph[dim]), ph[1-dim] - pl[1-dim])
+    pd = min(min(pl[dim], ph[dim]) - pa[dim], ph[1-dim] - pl[1-dim])
 
     medp = quickmedian([p[dim] for p in P])
     Pright = [p for p in P if p[dim] > medp] # Desna polovica mnozice P glede na x
@@ -97,32 +97,126 @@ def case23left(P, dim=0):
         Pright = [p for p in P if p[dim] > medp] # Desna polovica mnozice P glede na x
     return (pa,pl,ph), pd
 
+def case23right(P, dim=0):
+    # Case 2 where pa = p1 (rightmost point) => (pb,pc) is type-V
+    pa = max(P, key=lambda x: x[dim])
+    pl = min(P, key=lambda x: (x[1-dim], -x[dim]))
+    ph = max(P, key=lambda x: (x[1-dim], x[dim]))
+    pd = min(pa[dim] - max(pl[dim], ph[dim]), ph[1-dim] - pl[1-dim])
+
+    medp = quickmedian([p[dim] for p in P])
+    Pleft = [p for p in P if p[dim] < medp] # Desna polovica mnozice P glede na x
+    while len(Pleft) > 1 or (len(Pleft) == 0 and len(P) > 1):
+        if len(Pleft) == 0:
+            # Robni primer, kjer so vse tocke v levi polovici, ali pa imajo
+            # x-koordinato enako mediani
+            # Najprej poizkusimo za Pmedian
+            Pmedian = [p for p in P if p[dim] == medp]
+            l, h = getLowHigh(Pmedian, 1-dim)
+            pl, ph, pd = getBestDist(l, h, pa, pl, ph, pd, dim)
+            P = [p for p in P if p[dim] > medp]
+        else:
+            l, h = getLowHigh(Pleft, 1-dim)
+            pl, ph, pd = getBestDist(l, h, pa, pl, ph, pd, dim)
+            if pa[dim] - medp < h[1-dim] - l[1-dim]:
+                # Vemo, da se lahko pomaknemo strogo desno
+                P = Pleft
+            else:
+                # Nadaljujemo iskanje v levi polovici P
+                P = [p for p in P if p[dim] >= medp]
+        medp = quickmedian([p[dim] for p in P]) # Mediana po x
+        Pleft = [p for p in P if p[dim] < medp] # Desna polovica mnozice P glede na x
+    return (pa,pl,ph), pd
+
+def getPnext(P, medp, dim, smallest):
+    return [p for p in P if p[dim] > medp] if smallest else [p for p in P if p[dim] < medp]
+
+def case23(P, dim=0, smallest=True):
+    '''Case 2 za dim=0 in Case 3 za dim=1.
+    Če smallest, potem je pa tocka z najmanjso dim-koordinato (za dim=0 je pa=pn),
+    sicer je tocka z najvecjo dim-koordinato (za dim=0 je pa=p1).'''
+    # pa je skrajna tocka v dim => (pb,pc) je type-V
+    pa = min(P, key=lambda x: x[dim]) if smallest else max(P, key=lambda x: x[dim])
+    pl = min(P, key=lambda x: (x[1-dim], -x[dim]))
+    ph = max(P, key=lambda x: (x[1-dim], x[dim]))
+    pd = min(min(abs(pa[dim]-pl[dim]), abs(pa[dim]-ph[dim])), ph[1-dim]-pl[1-dim])
+
+    medp = quickmedian([p[dim] for p in P])
+    Pnext = getPnext(P, medp, dim, smallest) # Razdelimo P na pol, vzamemo "naslednjo" polovico
+    while len(Pnext) > 1 or (len(Pnext) == 0 and len(P) > 2):
+        if len(Pnext) == 0:
+            # Robni primer, kjer so vse tocke v "non-next" polovici
+            # Ker je teh tock >2, imamo vec vrednosti z dim-koordinato enako medp
+            # Najprej poizkusimo za Pmedian
+            Pmedian = [p for p in P if p[dim] == medp]
+            l, h = getLowHigh(Pmedian, 1-dim)
+            pl, ph, pd = getBestDist(l, h, pa, pl, ph, pd, dim)
+            P = [p for p in P if p[dim] < medp] if smallest else [p for p in P if p[dim] > medp]
+        else:
+            # Preverimo, ce je boljsa resitev v Pnext
+            l, h = getLowHigh(Pnext, 1-dim)
+            pl, ph, pd = getBestDist(l, h, pa, pl, ph, pd, dim)
+            if abs(pa[dim]-medp) < h[1-dim]-l[1-dim]:
+                # Vemo, da se lahko pomaknemo strogo v smeri "next"
+                P = Pnext
+            else:
+                # Nadaljujemo iskanje v "non-next" polovici P
+                P = [p for p in P if p[dim] <= medp] if smallest else [p for p in P if p[dim] >= medp]
+        medp = quickmedian([p[dim] for p in P]) # Mediana po x
+        Pnext = getPnext(P, medp, dim, smallest) # Desna polovica mnozice P glede na x
+    return (pa,pl,ph), pd
+
 P = [(0,0), (1,1), (1.5,-1), (5, -5), (6, 5), (7, -1), (8, 1)]
 print(case23left(P))
+print(case23(P))
 P1 = [(y,x) for x,y in P]
 print(case23left(P1, 1))
+print(case23(P1, 1))
+P2 = [(-x,y) for x,y in P]
+print(case23right(P2))
+print(case23(P2, smallest=False))
 print()
 
 P = [(0,0), (1,1), (1,-1), (1,3)]
 print(case23left(P))
+print(case23(P))
 P1 = [(y,x) for x,y in P]
 print(case23left(P1, 1))
+print(case23(P1, 1))
+P2 = [(-x,y) for x,y in P]
+print(case23right(P2))
+print(case23(P2, smallest=False))
 print()
 
 P = [(0,0), (2,10), (3,-10), (5,1), (5,-1), (5,3)]
 print(case23left(P))
+print(case23(P))
 P1 = [(y,x) for x,y in P]
 print(case23left(P1, 1))
+print(case23(P1, 1))
+P2 = [(-x,y) for x,y in P]
+print(case23right(P2))
+print(case23(P2, smallest=False))
 print()
 
 P = [(0,0), (1,-10), (2,10), (100,-5), (101,0), (102,5), (103,0), (104,0), (105,0)]
 print(case23left(P))
+print(case23(P))
 P1 = [(y,x) for x,y in P]
 print(case23left(P1, 1))
+print(case23(P1, 1))
+P2 = [(-x,y) for x,y in P]
+print(case23right(P2))
+print(case23(P2, smallest=False))
 print()
 
 P = [(0,0), (1,-10), (2,10), (100,3), (100,2), (100,1), (100,-3)]
 print(case23left(P))
+print(case23(P))
 P1 = [(y,x) for x,y in P]
 print(case23left(P1, 1))
+print(case23(P1, 1))
+P2 = [(-x,y) for x,y in P]
+print(case23right(P2))
+print(case23(P2, smallest=False))
 print()
