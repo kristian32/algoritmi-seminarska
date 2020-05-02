677a9272ad2b5477aa37c5a363099f84b2fb3fd4
Samo <samo.metlicar@hotmail.com>
2020-05-02 17:16:33 +0200

    Update L_inf.py
    Added support for case3 (swapping x and y dimensions).

--- L_inf.py
+++ L_inf.py
@@ -22,10 +22,10 @@ def getLowHigh(xs, dim=0):
             high = x
     return low, high
 
-def getBestDist(l, h, pa, pl, ph, pd):
-    dist_lh = min(min(l[0],h[0])-pa[0], h[1] - l[1])
-    dist_lph = min(min(l[0],ph[0])-pa[0], ph[1]-l[1])
-    dist_plh = min(min(pl[0],h[0])-pa[0], h[1]-pl[0])
+def getBestDist(l, h, pa, pl, ph, pd, dim=0):
+    dist_lh = min(min(l[dim],h[dim])-pa[dim], h[1-dim] - l[1-dim])
+    dist_lph = min(min(l[dim],ph[dim])-pa[dim], ph[1-dim]-l[1-dim])
+    dist_plh = min(min(pl[dim],h[dim])-pa[dim], h[1-dim]-pl[dim])
     dist = max(pd, dist_lh, dist_lph, dist_plh)
     if dist_lh == dist:
         # l in h sta nova najbolja izbira
@@ -66,48 +66,63 @@ def case1(P):
 def case4(P):
     return case14(P, 1)
 
-def case2left(P):
+def case23left(P, dim=0):
     # Case 2 where pa = pn (leftmost point) => (pb,pc) is type-V
-    pa = min(P)
-    pl = min(P, key=lambda x: (x[1], -x[0]))
-    ph = max(P, key=lambda x: (x[1], x[0]))
-    pd = min(min(pl[0], ph[0]), ph[1] - pl[1])
+    pa = min(P, key=lambda x: x[dim])
+    pl = min(P, key=lambda x: (x[1-dim], -x[dim]))
+    ph = max(P, key=lambda x: (x[1-dim], x[dim]))
+    pd = min(min(pl[dim], ph[dim]), ph[1-dim] - pl[1-dim])
 
-    medp = quickmedian([p[0] for p in P])
-    Pright = [p for p in P if p[0] > medp] # Desna polovica mnozice P glede na x
+    medp = quickmedian([p[dim] for p in P])
+    Pright = [p for p in P if p[dim] > medp] # Desna polovica mnozice P glede na x
     while len(Pright) > 1 or (len(Pright) == 0 and len(P) > 1):
         if len(Pright) == 0:
             # Robni primer, kjer so vse tocke v levi polovici, ali pa imajo
             # x-koordinato enako mediani
             # Najprej poizkusimo za Pmedian
-            Pmedian = [p for p in P if p[0] == medp]
-            l, h = getLowHigh(Pmedian, 1)
-            pl, ph, pd = getBestDist(l, h, pa, pl, ph, pd)
-            P = [p for p in P if p[0] < medp]
+            Pmedian = [p for p in P if p[dim] == medp]
+            l, h = getLowHigh(Pmedian, 1-dim)
+            pl, ph, pd = getBestDist(l, h, pa, pl, ph, pd, dim)
+            P = [p for p in P if p[dim] < medp]
         else:
-            l, h = getLowHigh(Pright, 1)
-            pl, ph, pd = getBestDist(l, h, pa, pl, ph, pd)
-            if medp - pa[0] < h[1] - l[1]:
+            l, h = getLowHigh(Pright, 1-dim)
+            pl, ph, pd = getBestDist(l, h, pa, pl, ph, pd, dim)
+            if medp - pa[dim] < h[1-dim] - l[1-dim]:
                 # Vemo, da se lahko pomaknemo strogo desno
                 P = Pright
             else:
                 # Nadaljujemo iskanje v levi polovici P
-                P = [p for p in P if p[0] <= medp]
-        medp = quickmedian([p[0] for p in P]) # Mediana po x
-        Pright = [p for p in P if p[0] > medp] # Desna polovica mnozice P glede na x
+                P = [p for p in P if p[dim] <= medp]
+        medp = quickmedian([p[dim] for p in P]) # Mediana po x
+        Pright = [p for p in P if p[dim] > medp] # Desna polovica mnozice P glede na x
     return (pa,pl,ph), pd
 
 P = [(0,0), (1,1), (1.5,-1), (5, -5), (6, 5), (7, -1), (8, 1)]
-print(case2left(P))
+print(case23left(P))
+P1 = [(y,x) for x,y in P]
+print(case23left(P1, 1))
+print()
 
 P = [(0,0), (1,1), (1,-1), (1,3)]
-print(case2left(P))
+print(case23left(P))
+P1 = [(y,x) for x,y in P]
+print(case23left(P1, 1))
+print()
 
 P = [(0,0), (2,10), (3,-10), (5,1), (5,-1), (5,3)]
-print(case2left(P))
+print(case23left(P))
+P1 = [(y,x) for x,y in P]
+print(case23left(P1, 1))
+print()
 
 P = [(0,0), (1,-10), (2,10), (100,-5), (101,0), (102,5), (103,0), (104,0), (105,0)]
-print(case2left(P))
+print(case23left(P))
+P1 = [(y,x) for x,y in P]
+print(case23left(P1, 1))
+print()
 
 P = [(0,0), (1,-10), (2,10), (100,3), (100,2), (100,1), (100,-3)]
-print(case2left(P))
+print(case23left(P))
+P1 = [(y,x) for x,y in P]
+print(case23left(P1, 1))
+print()
