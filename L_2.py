8941b50a9bb965a8e73bd59c46807710753e57f2
Samo <samo.metlicar@hotmail.com>
2020-05-03 17:52:16 +0200

    Update L_2.py
    Popravek glede indeksiranja.

--- L_2.py
+++ L_2.py
@@ -28,15 +28,16 @@ class L2:
             opt_pair = None
             opt_dist = -1
             P.sort(key=lambda p: self.L2metric(pa, p))
-            I = [1, n]
+            I = [0, n-1]
             while I[1]-I[0] > 1:
+                # TODO - premisli, ce je razlika = 1, torej imamo 2 elementa se
                 # Izracunamo mediano zaporednih indeksov I ... mediana je tukaj enaka mean
                 i = ceil((I[0]+I[1]) / 2)
                 dpi = self.L2metric(pa, P[i]) # d(pa, pi)
 
                 # Izracunajmo sedaj konveksno ovojnico Pi = P[i:]
-                U, L = grahamHull(P[(i-1):I[1]])
-                if I[1] < n:
+                U, L = grahamHull(P[i:(I[1]+1)])
+                if I[1] < n-1:
                     # Zdruzimo C in Cright, da dobimo CH(Pi)
                     U, L = union(U, L, Uright, Lright)
                 
@@ -53,7 +54,7 @@ class L2:
                     # Torej vemo, da resitev ni v levi polovici P[:i], temvec
                     # je v desni, ce je se nismo nasli ... pomaknemo se v desno polovico
                     I[0] = i
-                    Uright = None
+                    Uright = None # Zgolj, da javi napako, ce algoritem ne bi deloval pravilno
                     Lright = None
                 else:
                     # d(pa, P[i]) >= d(pb, pc) ... nadaljujemo iskanje v levi polovici
