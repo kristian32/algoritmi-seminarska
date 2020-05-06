a3bfb9b4c8f0334cc63f54e734edb16f950c616a
Samo <samo.metlicar@hotmail.com>
2020-05-06 22:17:16 +0200

    L_inf.py & quickmedian.py overhaul
    Spremembe za `quickmedian.py`:
    -`quickselect` sedaj sprejme moznost kljuca za primerjanje vrednosti;
    -`quickmedian` sedaj deluje z 2D točkami namesto 1D vrednostmi, pravtako sprejme ključ za primerjave. Če je seznam sode dolžine, vrne najmanjši element desne polovice.
    
    Spremembe za `L_inf.py`:
    -Najdeni stevilni buggi;
    -funkcija `case23` popolnoma spremenjena, sedaj deluje v skladu z opisom v clanku in vedno vrne resitev za dolocen "case" in ne posplošuje rešitve (ohranja razmerje H in V kot pripadajo case-u);
    -odstranjene nekatere pomožne funkcije, ki niso več v uporabi.

--- L_inf.py
+++ L_inf.py
@@ -6,26 +6,14 @@ class Linf:
     def __init__(self):
         self.name = "3-dispersion in Linf"
         self.metric = "$L_{\\infty}$"
+    
+    @staticmethod
+    def LinfMetric(x, y):
+        return max(abs(x[0]-y[0]), abs(x[1]-y[1]))
 
-    def getBestDist(self, l, h, pa, pl, ph, pd, dim=0):
-        dist_lh = min(min(abs(pa[dim]-l[dim]), abs(pa[dim]-h[dim])), h[1-dim]-l[1-dim])
-        dist_lph = min(min(abs(pa[dim]-l[dim]), abs(pa[dim]-ph[dim])), ph[1-dim]-l[1-dim])
-        dist_plh = min(min(abs(pa[dim]-pl[dim]), abs(pa[dim]-h[dim])), h[1-dim]-pl[1-dim])
-        dist = max(pd, dist_lh, dist_lph, dist_plh)
-        if dist_lh == dist:
-            # l in h sta nova najbolja izbira
-            pl = l
-            ph = h
-            pd = dist
-        elif dist_lph == dist:
-            # l in ph je nova najboljsa izbira
-            pl = l
-            pd = dist
-        elif dist_plh == dist:
-            # pl in h je nova najboljsa izbira
-            ph = h
-            pd = dist
-        return pl, ph, pd
+    @staticmethod
+    def getMaxMinDist(S):
+        return min(Linf.LinfMetric(S[0],S[1]), Linf.LinfMetric(S[0],S[2]), Linf.LinfMetric(S[1],S[2]))
 
     def case14(self, P, dim=0):
         maxp = P[dim]
@@ -45,58 +33,70 @@ class Linf:
                 pm = p
         return (minp, pm, maxp)
 
-    def getPnext(self, P, medp, dim, smallest):
-        return [p for p in P if p[dim] > medp] if smallest else [p for p in P if p[dim] < medp]
-
     def case23(self, P, dim=0, smallest=True):
-        '''Case 2 za dim=0 in Case 3 za dim=1.
-        Če smallest, potem je pa tocka z najmanjso dim-koordinato (za dim=0 je pa=pn),
-        sicer je tocka z najvecjo dim-koordinato (za dim=0 je pa=p1).'''
-        fact = 1 if smallest else -1 # Factor for multiplications
-        pa = min(P, key=lambda x: x[dim]) if smallest else max(P, key=lambda x: x[dim])# pa je skrajna tocka v dim => (pb,pc) je type-V
-        pl = min(P, key=lambda x: (x[1-dim], -fact * x[dim]))
-        ph = max(P, key=lambda x: (x[1-dim], fact * x[dim]))
-        pd = min(min(abs(pa[dim]-pl[dim]), abs(pa[dim]-ph[dim])), ph[1-dim]-pl[1-dim])
+        '''Case 2: imamo bodisi 2x H 1x V (dim=0), ali ravno obratno (dim=1).
+        pa je bodisi najmanjsa vrednost (smallest), ali najvecja (!smallest).'''
 
-        medp = quickmedian([p[dim] for p in P])
-        Pnext = self.getPnext(P, medp, dim, smallest) # Razdelimo P na pol, vzamemo "naslednjo" polovico
-        while len(Pnext) > 1 or (len(Pnext) == 0 and len(P) > 2):
-            if len(Pnext) == 0:
-                # Robni primer, kjer so vse tocke v "non-next" polovici
-                # Ker je teh tock >2, imamo vec vrednosti z dim-koordinato enako medp
-                # Najprej poizkusimo za Pmedian
-                Pmedian = [p for p in P if p[dim] == medp]
-                # Vse tocke iz Pmedian imajo isto dim-koordinato
-                l = min(Pmedian, key=lambda x: x[1-dim])
-                h = max(Pmedian, key=lambda x: x[1-dim])
-                pl, ph, pd = self.getBestDist(l, h, pa, pl, ph, pd, dim)
-                P = [p for p in P if p[dim] < medp] if smallest else [p for p in P if p[dim] > medp]
-                if len(P) < 2:
-                    break
+        assert len(P) >= 3 # Ce nimamo vsaj 3 tock, problem ni smiselen
+
+        fact = 1 if smallest else -1 # Faktor ... 1 = pa je min, -1 = pa je max, torej samo prezrcalimo tocke preko y-osi (dim=0) oz. x-osi (dim=1)
+        key_dim = lambda x: (fact * x[dim], x[1-dim]) # Kljuc za primerjanje v dimenziji (dim)
+        key_max = lambda x: (x[1-dim], fact * x[dim]) # Kljuc za primerjanje v dimenziji (1-dim) za max
+        key_min = lambda x: (x[1-dim], -fact * x[dim]) # Kljuc za primerjanje v dimenziji (1-dim) za min
+        
+        pa = min(P, key=key_dim) # Zacetna tocka
+
+        Pi = P # Pi je na zacetku kar celotna množica
+        pl = pa
+        ph = pa
+
+        hright = None
+        lright = None
+
+        wentRight = False
+        
+        while len(P) > 1:
+            pi = quickmedian(P, key_dim)
+            pi_key = key_dim(pi)
+            Pi = [pi] + [p for p in P if key_dim(p) > pi_key] # Pi ... tocke, ki so od pa oddaljene vsaj toliko kot pi | ce obstaja vec tock enakih pi, jih pobrisemo
+
+            # Posodobimo / pobrisemo pl/ph, ce nista znotraj Pi
+            if pi_key > key_dim(pl):
+                pl = pi if lright is None else lright
+            if pi_key > key_dim(ph):
+                ph = pi if hright is None else hright
+            
+            # Poiscemo ekstremni tocki v (1-dim) dimenziji
+            l = min(Pi, key=key_min)
+            h = max(Pi, key=key_max)
+            pl = l if key_min(l) < key_min(pl) else pl
+            ph = h if key_max(h) > key_max(ph) else ph
+
+            # Sedaj se moramo odlociti, kako nadaljevati
+            diam = ph[1-dim]-pl[1-dim] # diam(Pi)
+            dpi = abs(pa[dim]-pi[dim]) # d(pa,pi)
+            if dpi < diam:
+                # Iskanje se bo nadaljevalo v desni polovici Pi
+                P = Pi
+                wentRight = True
             else:
-                # Preverimo, ce je boljsa resitev v Pnext
-                l = min(Pnext, key=lambda x: (x[1-dim], -fact * x[dim])) # Zelimo najnizjo tocko ... ce jih je vec, tisto, ki je najdlje od pa horizontalno
-                h = max(Pnext, key=lambda x: (x[1-dim], fact * x[dim]))
-                pl, ph, pd = self.getBestDist(l, h, pa, pl, ph, pd, dim)
-                if abs(pa[dim]-medp) < h[1-dim]-l[1-dim]:
-                    # Vemo, da se lahko pomaknemo strogo v smeri "next"
-                    P = Pnext
-                else:
-                    # Nadaljujemo iskanje v "non-next" polovici P
-                    P = [p for p in P if p[dim] <= medp] if smallest else [p for p in P if p[dim] >= medp]
-            medp = quickmedian([p[dim] for p in P]) # Mediana po x
-            Pnext = self.getPnext(P, medp, dim, smallest) # Desna polovica mnozice P glede na x
-        return (pa,pl,ph)
-    
-    def LinfMetric(self, x, y):
-        return max(abs(x[0]-y[0]), abs(x[1]-y[1]))
+                # Iskanje se bo nadaljevalo v levi polovici Pi
+                P = [p for p in P if key_dim(p) < pi_key]
+                hright = ph
+                lright = pl
+                wentRight = False
+        if wentRight:
+            # Na zadnjem koraku smo zeleli iti desno, torej je d(pa,pi) < diam(Pi)
+            # Mozno je torej, da je optimalna resitev bila bolj desno od nase trenutne
+            return max([(pa,pl,ph), (pa,pl,hright), (pa,lright,ph), (pa,lright,hright)], key=Linf.getMaxMinDist)
+        else:
+            # Na zadnjem koraku smo sli v levo polovico, torej d(pa,pi) >= diam(Pi)
+            # V levi polovici je tocka P[0], preverimo, ce je boljsa od pl/ph
+            return max([(pa, pl, ph), (pa, pl, P[0]), (pa, P[0], ph)], key=Linf.getMaxMinDist)
 
-    def getMaxMinDist(self, S):
-        return min(self.LinfMetric(S[0],S[1]), self.LinfMetric(S[0],S[2]), self.LinfMetric(S[1],S[2]))
-    
     def three_dispersion(self, P):
         # Vrne 3-disperzijo za metriko Linf in mnozico 2-dimenzionalnih tock P
         # Izracuna disperzijo za vse mozne primere ter izbere najboljso moznost
         Slist = [self.case14(P, 0), self.case23(P, 0, True), self.case23(P, 0, False),
                 self.case23(P, 1, True), self.case23(P, 1, False), self.case14(P, 1)]
-        return max(Slist, key=lambda S: self.getMaxMinDist(S))
+        return max(Slist, key=Linf.getMaxMinDist)
--- quickmedian.py
+++ quickmedian.py
@@ -2,37 +2,38 @@
 
 import random
 
-def quickselect(xs, k):
+def quickselect(xs, k, key=lambda x: x):
     if len(xs) == 1:
         # Median of list with 1 element
         assert k == 0
         return xs[0]
     
     pivot = random.choice(xs)
+    pivot_key = key(pivot)
 
     left = []
     right = []
     pivots = []
     for x in xs:
-        if x < pivot:
+        x_key = key(x)
+        if x_key < pivot_key:
             left.append(x)
-        elif x == pivot:
+        elif x_key == pivot_key:
             pivots.append(x)
         else:
             right.append(x)
     
     if k < len(left):
         # Median is in left
-        return quickselect(left, k)
+        return quickselect(left, k, key)
     elif k < len(left) + len(pivots):
         # Pivot is the median
         return pivots[0]
     else:
         # Median is in right
-        return quickselect(right, k - len(left) - len(pivots))
+        return quickselect(right, k - len(left) - len(pivots), key)
 
-def quickmedian(xs):
-    if len(xs) % 2 == 0:
-        return 0.5 * (quickselect(xs, len(xs) // 2) + quickselect(xs, len(xs) // 2 - 1))
-    else:
-        return quickselect(xs, len(xs) // 2)
+def quickmedian(xs, key=lambda x: x):
+    '''Vrnemo mediano, ce je xs lihe dolzine. Ce je sode dolzine, vrnemo desno
+    tocko izmed srednjih dveh.'''
+    return quickselect(xs, len(xs) // 2, key)
