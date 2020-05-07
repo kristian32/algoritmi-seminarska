fcbd08397a77c2753011716cf77e51dff6b44c2c
Samo <samo.metlicar@hotmail.com>
2020-05-07 02:39:28 +0200

    L_2.py overhaul
    Posodobljena funkcija `three_dispersion` v `L_2.py`:
    -popravljen bug, ki ga je povzrocal premik v desno na `i+1` namesto na `i`, kar je lahko povzrocilo, da se ne mores vrniti na optimalno resitev, ki jo zato izgubis;
    -koda napisana bolj skladno s clankom, odstranjeni nepotrebni izracuni in primerjave.

--- L_2.py
+++ L_2.py
@@ -15,23 +15,24 @@ class L2:
     def getMaxMinDist(self, S):
         return min(self.L2metric(S[0],S[1]), self.L2metric(S[0],S[2]), self.L2metric(S[1],S[2]))
 
-    
-    def three_dispersion(self, P):
-        assert len(P) >= 3 # Iscemo 3 tocke, ce jih toliko sploh ni, naloga nima smisla
+    def three_dispersion(self, Porg):
+        assert len(Porg) >= 3 # Iscemo 3 tocke, ce jih toliko sploh ni, naloga nima smisla
 
-        opt_S = (P[0], P[1], P[2])
+        opt_S = (Porg[0], Porg[1], Porg[2])
         opt_d = self.getMaxMinDist(opt_S)
 
-        Porg = [p for p in P] # Naredimo kopijo P, ki ga bomo nato cel cas sortirali
+        P = [p for p in Porg] # Naredimo kopijo P, ki ga bomo nato cel cas sortirali
         n = len(P) # oznacimo stevilo tock z n
 
         # Za vsak pa v P izracunamo maxmin S
         for pa in Porg:
-            opt_pair = None
-            opt_dist = -1
+            Uright = None
+            Lright = None
+
             P.sort(key=lambda p: self.L2metric(pa, p))
             I = [0, n-1]
-            while I[1]-I[0] > -1:
+            fromLeft = False
+            while I[1]-I[0] > 0 or (fromLeft and I[1]-I[0] > -1):
                 # Izracunamo mediano zaporednih indeksov I ... mediana je tukaj enaka mean
                 i = ceil((I[0]+I[1]) / 2)
                 dpi = self.L2metric(pa, P[i]) # d(pa, pi)
@@ -45,25 +46,17 @@ class L2:
                 
                 if i == n-1:
                     # Smo na najbolj desni tocki, torej je I[0] = n-2, I[1] = n-1
-                    d = self.getMaxMinDist((pa, P[-2], P[-1]))
-                    if d > opt_d:
-                        opt_pair = (pa, P[-2], P[-1])
-                        opt_d = d
+                    pb, pc = P[-2:]
                     break
                 
                 (pb,pc), d = diameter(U, L)
-
-                # Preverimo najprej, ce smo nasli novo najboljso vrednost
-                dreali = self.getMaxMinDist((pa,pb,pc)) # Tocna maxmin razdalja te trojke
-                if dreali > opt_dist:
-                    opt_pair = (pb,pc)
-                    opt_dist = dreali
 
                 if dpi < d:
                     # Razdalja do P[i] je strogo manjsa od razdalje d(pb,pc)
                     # Torej vemo, da resitev ni v levi polovici P[:i], temvec
                     # je v desni, ce je se nismo nasli ... pomaknemo se v desno polovico
-                    I[0] = i+1
+                    I[0] = i
+                    fromLeft = False
                 else:
                     # d(pa, P[i]) >= d(pb, pc) ... nadaljujemo iskanje v levi polovici
                     # Za nadaljne izracune bomo potrebovali trenutno konveksno ovojnico,
@@ -71,10 +64,11 @@ class L2:
                     Uright = U
                     Lright = L
                     I[1] = i-1
-            # Nasli smo optimalen par opt_pair za pa
-            # Primerjajmo (pa, opt_pair) sedaj z opt_S
-            dpa = self.getMaxMinDist((pa, opt_pair[0], opt_pair[1]))
+                    fromLeft = True
+            # Nasli smo optimalen par za pa, to sta (pb, pc)
+            # Preverimo, ce je trojka (pa,pb,pc) najboljsa resitev do sedaj
+            dpa = self.getMaxMinDist((pa,pb,pc))
             if dpa > opt_d:
-                opt_S = (pa, opt_pair[0], opt_pair[1])
+                opt_S = (pa,pb,pc)
                 opt_d = dpa
         return opt_S
