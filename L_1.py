54401628d3aac456c66313d5c7ba98c001968317
Samo <samo.metlicar@hotmail.com>
2020-05-10 18:43:48 +0200

    Finalizacija
    Odstranil prejšnji `L_1.py` in preimenoval `L_1v2.py` v `L_1.py`. Odstranil mapo `inputs` in vse njene elemente, ker se niso uporabljali.

--- L_1.py
+++ L_1.py
@@ -1,6 +1,7 @@
 # 3-Dispersion in L_1
 
 from math import sin, cos, radians
+from quickmedian import quickmedian
 
 class L1:
     def __init__(self):
@@ -10,6 +11,16 @@ class L1:
     @staticmethod
     def L_1_metric(p1, p2):  # manhattan distance
         return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+    @staticmethod
+    def getMaxMinDistList(S):
+        SS = [L1.L_1_metric(S[0], S[1]), L1.L_1_metric(S[0], S[2]), L1.L_1_metric(S[1], S[2])]
+        SS.sort(reverse=True)
+        return SS
+
+    @staticmethod
+    def getMaxMinDist(S):
+        return min(L1.L_1_metric(S[0], S[1]), L1.L_1_metric(S[0], S[2]), L1.L_1_metric(S[1], S[2]))
 
     @staticmethod
     def rotate_points(P, alpha):
@@ -27,64 +38,81 @@ class L1:
             return pu[1] <= pv[1]
         return pv[1] <= pu[1]
 
-    def diameter_L1(self, Pi):
-        p1, p2 = self.diameter_extremes(Pi)
-        return self.L_1_metric(p1, p2)
+    def get_S(self, P, smallest):
 
-    def diameter_extremes(self, Pi):
-        return Pi[0], Pi[-1]
+        assert len(P) >= 3  # Ce nimamo vsaj 3 tock, problem ni smiselen
 
-    def get_S(self, P, pa_index):
-        pa = P[pa_index]  # we assume P is sorted descending by value x
-        opt_i = 0
-        opt_i_index = 0
+        fact = 1 if smallest else -1  # Faktor ... 1 = pa je min, -1 = pa je max, torej samo prezrcalimo tocke preko y-osi (dim=0) oz. x-osi (dim=1)
+        pa = max(P, key=lambda x: fact * x[0])  # Zacetna tocka
 
-        for i in range(1, len(P)):
-            if pa_index == -1:
-                Pi = P[0:i]
-                pi = P[i]
+        key_dim = lambda x:  L1.L_1_metric(pa, x)  # Kljuc za primerjanje v dimenziji (dim)
+        key1 = lambda x: x[1] # Uporabljamo samo zato, ker je sicer VSC pobarval spodaj kot napako, ceprav je ni bilo, in je bilo nadlezno
+
+        pl = pa
+        ph = pa
+
+        wentRight = False
+
+        pcD = max(P, key=lambda x: x[0]) # vedno najbolj desna tocka
+        hright = None
+        lright = None
+
+        while len(P) > 1:
+            pi = quickmedian(P, key_dim)
+            pi_key = key_dim(pi)
+            Pi = [pi] + [p for p in P if key_dim(p) > pi_key]  # Pi ... tocke, ki so od pa oddaljene vsaj toliko kot pi | ce obstaja vec tock enakih pi, jih pobrisemo
+
+            dpi = L1.L_1_metric(pa, pi)  # d(pa,pi)
+
+            # type U
+            pbU = min(Pi, key=lambda x: x[1])
+            if lright is not None and key1(lright) < pbU[1]:
+                pbU = lright
+            pcU = max(Pi, key=lambda x: x[1])
+            if hright is not None and key1(hright) > pcU[1]:
+                pcU = hright
+            # type D
+            pbD = pi
+
+            U = L1.L_1_metric(pbU, pcU)
+            D = L1.L_1_metric(pbD, pcD)
+
+            if U > D:
+                pl = pbU
+                ph = pcU
+                diam = U
             else:
-                Pi = P[-i:]
-                pi = P[-i]
+                pl = pbD
+                ph = pcD
+                diam = D
 
-            v = min(self.L_1_metric(pa, pi), self.diameter_L1(Pi))
-            if v > opt_i:
-                opt_i = v
-                opt_i_index = i
+            if dpi < diam:
+                # Iskanje se bo nadaljevalo v desni polovici Pi
+                P = Pi
+                wentRight = True
+            else:
+                # Iskanje se bo nadaljevalo v levi polovici Pi
+                P = [p for p in P if key_dim(p) < pi_key]
+                hright = pcU
+                lright = pbU
+                wentRight = False
 
-        if pa_index == -1:
-            p1, p2 = self.diameter_extremes(P[0:opt_i_index])
+        if wentRight and hright is not None:
+            # Na zadnjem koraku smo zeleli iti desno, torej je d(pa,pi) < diam(Pi)
+            # Mozno je torej, da je optimalna resitev bila bolj desno od nase trenutne
+            return max([(pa, pl, ph), (pa, pl, hright), (pa, lright, ph), (pa, lright, hright)], key=L1.getMaxMinDist)
         else:
-            p1, p2 = self.diameter_extremes(P[-opt_i_index:])
-
-        index = 0  # typeD
-        if self.is_type_U(p1, p2):
-            index = 1
-
-        if p1[index] > p2[index]:
-            min_pi = p2
-            max_pi = p1
-        else:
-            min_pi = p1
-            max_pi = p2
-
-        if pa_index == -1:
-            return opt_i, (pa, min_pi, max_pi)
-
-        return opt_i, (pa, max_pi, min_pi)
-
-    def getMaxMinDist(self, S):
-        return min(self.L_1_metric(S[0], S[1]), self.L_1_metric(S[0], S[2]), self.L_1_metric(S[1], S[2]))
+            # Na zadnjem koraku smo sli v levo polovico, torej d(pa,pi) >= diam(Pi)
+            # V levi polovici je tocka P[0], preverimo, ce je boljsa od pl/ph
+            return max([(pa, pl, ph), (pa, pl, P[0]), (pa, P[0], ph)], key=L1.getMaxMinDist)
 
     def three_dispersion(self, P):
-        val, S = [], []
+        val = []
         angles = [-45, -135, -225, -315]
         for angle in angles:
-            for pa_index in [0, -1]:  # min or max x value
+            for pa_index in [True, False]:  # min or max x value
                 P_rot = self.rotate_points(P, angle)
-                P_rot.sort(reverse=True)
-                v, s = self.get_S(P_rot, pa_index)  # sorted by x descending?
-                val.append(v)
-                S.append(self.rotate_points(s, -angle))
-
-        return max(S, key=lambda Si: self.getMaxMinDist(Si))
+                s = self.get_S(P_rot, pa_index)  # sorted by x descending?
+                s_rot = self.rotate_points(s, -angle)
+                val.append(s_rot)
+        return max(val, key=lambda x: self.getMaxMinDist(x))
